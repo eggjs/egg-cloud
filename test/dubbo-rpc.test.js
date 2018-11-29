22a291aadbd1e22a1f1adf5eb61425bc694e6dae
gxcsoccer <gxcsoccer@126.com>
2018-11-30 02:53:22 +0800

    feat: add egg-dubbo-tracer

--- test/dubbo-rpc.test.js
+++ test/dubbo-rpc.test.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const mock = require('egg-mock');
+const assert = require('assert');
 const sleep = require('mz-modules/sleep');
 
 describe('test/dubbo-rpc.test.js', () => {
@@ -8,6 +11,7 @@ describe('test/dubbo-rpc.test.js', () => {
   before(async function() {
     app = mock.app({
       baseDir: 'apps/dubbo-rpc-test',
+      framework: true,
     });
     await app.ready();
     await sleep(2000);
@@ -23,8 +27,8 @@ describe('test/dubbo-rpc.test.js', () => {
       .expect(200);
   });
 
-  it('should invoke echoPerson', done => {
-    app.rpcRequest('eggjs.demo.DemoService')
+  it('should invoke echoPerson', async function() {
+    await app.rpcRequest('eggjs.demo.DemoService')
       .invoke('echoPerson')
       .send([{
         name: '宗羽',
@@ -37,6 +41,12 @@ describe('test/dubbo-rpc.test.js', () => {
         address: 'C 空间',
         id: 68955,
         salary: 10000000,
-      }, done);
+      });
+
+    const data = fs.readFileSync(path.join(app.config.baseDir, 'logs/dubbo-rpc-test/opentracing.log'), 'utf8');
+    const spans = data.trim().split('\n').map(item => {
+      return JSON.parse(item);
+    });
+    assert(spans && spans.length === 4);
   });
 });
